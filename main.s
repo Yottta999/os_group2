eae7ebebce3579f2765d498580aeefa8fb6b61b7
Yottta999 <yota.a0929@icloud.com>
2025-11-12 15:46:58 +0900

    Update main.s

--- main.s
+++ main.s
@@ -1,139 +1,145 @@
+.section .text
+.even
+MAIN:
+    move.w #0x0000, %SR           | USER MODE
+    lea.l USR_STK_TOP, %SP        | スタック設定
+
+    ** タイマー初期化 **
+    move.l #SYSCALL_NUM_RESET_TIMER, %D0
+    trap #0
+    move.l #SYSCALL_NUM_SET_TIMER, %D0
+    move.w #10000, %D1
+    move.l #TT, %D2
+    trap #0
+
+MAIN_LOOP:
+    bra MAIN_LOOP                  | メインはタイマー割り込みで描画
+
+****************************************************************
+*** タイマー割り込み TT
+****************************************************************
 TT:
+    movem.l %d0-%d7/%a0-%a3,-(%sp)
 
-	movem.l %d0-%d7/%a0-%a3,-(%sp)
-
-	addq.w  #1, TTC
-	cmpi.w  #3600, TTC
-	beq     TTKILL
-
-	lea.l SECC, %a0
-	lea.l MINC, %a1
-	
-	addq.b  #1, (%a0)            | 秒++
-	cmpi.b  #60, (%a0)
-	bne    DISP_UPDATE
-	clr.b  (%a0)
-	
-	addq.b  #1, (%a1)            | 分++
-	cmpi.b  #60, (%a1)
-	bne   DISP_UPDATE
-	clr.b  (%a1)
+    addq.b  #1, SECC
+    cmpi.b  #60, SECC
+    bne DISP_UPDATE
+    clr.b   SECC
+    addq.b  #1, MINC
+    cmpi.b  #60, MINC
+    bne DISP_UPDATE
+    clr.b   MINC
 
 DISP_UPDATE:
-    lea.l   DIGITS,%a0        /* フォントテーブル先頭 */
-    lea.l   DISP_BUF,%a1        /* 出力先バッファ */
+    move.b MINC, %d0              | 分
+    move.b SECC, %d1              | 秒
 
-    move.b  MINC,%d0            /* 分 (例: 12) */
-    move.b  SECC,%d1            /* 秒 (例: 34) */
-
-    /** 分(十の位・一の位)を分解 **/
-    move.w  %d0,%d2
-    divu    #10,%d2             /* 商=十の位(%d2), 余り=一の位(%d3) */
-    move.w  %d2,%d3
+    move.w %d0, %d2
+    divu    #10, %d2              | D2=分十, D3=分一
+    move.w %d2, %d3
     swap    %d3
 
-    /** 秒(十の位・一の位)を分解 **/
-    move.w  %d1,%d4
-    divu    #10,%d4             /* 商=十の位(%d4), 余り=一の位(%d5) */
-    move.w  %d4,%d5
+    move.w %d1, %d4
+    divu    #10, %d4              | D4=秒十, D5=秒一
+    move.w %d4, %d5
     swap    %d5
 
-    /** 行ループ: 0〜5行 **/
-    moveq.l   #0,%d6
-    lea.l   DISP_BUF,%a2
-
+    moveq.l #0, %d6               | 行番号 (0〜4)
 ROW_LOOP:
-    move.b  %d2,%d7
-    bsr     DRAW_DIGIT_ROW      /* 分の十の位 */
-    move.b  %d3,%d7
-    bsr     DRAW_DIGIT_ROW      /* 分の一の位 */
-    move.b  #10,%d7
-    bsr     DRAW_DIGIT_ROW      /* コロン */
-    move.b  %d4,%d7
-    bsr     DRAW_DIGIT_ROW      /* 秒の十の位 */
-    move.b  %d5,%d7
-    bsr     DRAW_DIGIT_ROW      /* 秒の一の位 */
+    ** 各行に対して5文字を順に出力 **
+    move.b %d2, %d0
+    move.l %d6, %d1
+    bsr SEND_DIGIT_ROW            | 分十
 
-    move.b  #'\r',(%a2)+
-    move.b  #'\n',(%a2)+
+    move.b %d3, %d0
+    move.l %d6, %d1
+    bsr SEND_DIGIT_ROW            | 分一
 
-    addq.w  #1,%d6
-    cmpi.w  #6,%d6
-	blt.s   ROW_LOOP
+    move.b #10, %d0
+    move.l %d6, %d1
+    bsr SEND_DIGIT_ROW            | コロン
 
-TTKILL:
-	clr.w TTC
-	
+    move.b %d4, %d0
+    move.l %d6, %d1
+    bsr SEND_DIGIT_ROW            | 秒十
+
+    move.b %d5, %d0
+    move.l %d6, %d1
+    bsr SEND_DIGIT_ROW            | 秒一
+
+    ** 改行を出力 **
+    lea.l CRLF_BUF, %a0
+    move.l #SYSCALL_NUM_PUTSTRING, %d0
+    clr.l %d1
+    move.l %a0, %d2
+    move.l #2, %d3
+    trap #0
+
+    addq.l #1, %d6
+    cmpi.l #5, %d6
+    blt ROW_LOOP
+
     movem.l (%sp)+, %d0-%d7/%a0-%a3
     rts
 
-/****************************************************************
-*** サブルーチン: DRAW_DIGIT_ROW
-*** %d7 = 描画する文字番号 (0〜10)
-*** %d6 = 行番号 (0〜4)
-*** %a0 = FONT_TAB先頭
-*** %a2 = DISP_BUFの現在行位置
-****************************************************************/
-DRAW_DIGIT_ROW:
-    moveq   #25,%d0
-    mulu    %d7,%d0             /* 文字番号×20バイト */
-    move.l  %a0,%a3
-    adda.l  %d0,%a3             /* 文字の先頭へ */
-    adda.l  %d6,%a3             /* 行方向のオフセット */
+****************************************************************
+*** サブルーチン: SEND_DIGIT_ROW
+*** %d0 = 文字番号 (0〜9, 10=コロン)
+*** %d1 = 行番号 (0〜4)
+*** 各文字は 6文字×5行 固定配置
+****************************************************************
+SEND_DIGIT_ROW:
+    lea.l DIGITS, %a0             | 数字フォントテーブル
+    move.l %a0, %a2
 
-    moveq   #5,%d1              /* 4列分ループ(0〜3) */
-COL_LOOP:
-    move.b  (%a3)+,%d2
-    cmpi.b  #1,%d2
-    beq.s   PUT_SHARP
-    move.b  #' ',(%a2)+
-    bra.s   NEXT_COL
-PUT_SHARP:
-    move.b  #'#',(%a2)+
-NEXT_COL:
-    dbra    %d1,COL_LOOP
+    move.l %d0, %d3               | %d3 = 文字番号
+    mulu   #30, %d3               | 各文字 = 6×5 = 30バイト
+    adda.l %d3, %a2               | 該当文字へ
 
-    /** 各数字の間にスペース1つ **/
-    move.b  #' ',(%a2)+
+    move.l %d1, %d4               | %d4 = 行番号
+    mulu   #6, %d4                | 各行 = 6文字
+    adda.l %d4, %a2
+
+    move.l #SYSCALL_NUM_PUTSTRING, %d0
+    clr.l  %d1
+    move.l %a2, %d2
+    move.l #6, %d3                | 6文字出力
+    trap #0
     rts
 
+****************************************************************
+*** データ領域
+****************************************************************
 .section .data
-SECC:	.dc.b 0
-	.even
-MINC:	.dc.b 0
-	.even
-TTC:	.dc.w 0
-	.even
+SECC: .dc.b 0
+MINC: .dc.b 0
 
+****************************************************************
+*** 各数字: 幅6文字 (5 + 空白), 高さ5行
+****************************************************************
 DIGITS:
-/* 0 */
-dc.b 1,1,1,1,1, 1,0,0,0,1, 1,0,0,0,1, 1,0,0,0,1, 1,1,1,1,1
-/* 1 */
-dc.b 0,0,1,0,0, 0,1,1,0,0, 0,0,1,0,0, 0,0,1,0,0, 0,1,1,1,0
-/* 2 */
-dc.b 1,1,1,1,1, 0,0,0,0,1, 1,1,1,1,1, 1,0,0,0,0, 1,1,1,1,1
-/* 3 */
-dc.b 1,1,1,1,1, 0,0,0,0,1, 0,1,1,1,1, 0,0,0,0,1, 1,1,1,1,1
-/* 4 */
-dc.b 1,0,0,0,1, 1,0,0,0,1, 1,1,1,1,1, 0,0,0,0,1, 0,0,0,0,1
-/* 5 */
-dc.b 1,1,1,1,1, 1,0,0,0,0, 1,1,1,1,1, 0,0,0,0,1, 1,1,1,1,1
-/* 6 */
-dc.b 1,1,1,1,1, 1,0,0,0,0, 1,1,1,1,1, 1,0,0,0,1, 1,1,1,1,1
-/* 7 */
-dc.b 1,1,1,1,1, 0,0,0,0,1, 0,0,0,1,0, 0,0,1,0,0, 0,0,1,0,0
-/* 8 */
-dc.b 1,1,1,1,1, 1,0,0,0,1, 1,1,1,1,1, 1,0,0,0,1, 1,1,1,1,1
-/* 9 */
-dc.b 1,1,1,1,1, 1,0,0,0,1, 1,1,1,1,1, 0,0,0,0,1, 1,1,1,1,1
+| 0 |
+.ascii "##### " "#   # " "#   # " "#   # " "##### "
+| 1 |
+.ascii "    # " "    # " "    # " "    # " "    # "
+| 2 |
+.ascii "##### " "    # " "##### " "#     " "##### "
+| 3 |
+.ascii "##### " "    # " "##### " "    # " "##### "
+| 4 |
+.ascii "#   # " "#   # " "##### " "    # " "    # "
+| 5 |
+.ascii "##### " "#     " "##### " "    # " "##### "
+| 6 |
+.ascii "##### " "#     " "##### " "#   # " "##### "
+| 7 |
+.ascii "##### " "    # " "    # " "    # " "    # "
+| 8 |
+.ascii "##### " "#   # " "##### " "#   # " "##### "
+| 9 |
+.ascii "##### " "#   # " "##### " "    # " "##### "
+| コロン (10) |
+.ascii "      " "  ##  " "      " "  ##  " "      "
 
-/* コロン*/
-COLON:
-dc.b 0,0,0,0,0, 0,0,1,0,0, 0,0,0,0,0, 0,0,1,0,0, 0,0,0,0,0
-
-****************************************************************
-*** 初期値の無いデータ領域
-****************************************************************
-.section .bss
-DISP_BUF:
-.ds.b 256
+CRLF_BUF:
+.ascii "\r\n"
