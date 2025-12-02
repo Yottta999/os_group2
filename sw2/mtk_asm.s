b92d494063e57297223fa39f3e1050e34728b6c9
Aoto Murohara <136954211+muroblue@users.noreply.github.com>
2025-12-02 15:23:24 +0900

    hard_clockとinit_timerの追加
    枠組みの作成

--- sw2/mtk_asm.s
+++ sw2/mtk_asm.s
@@ -1,1 +1,20 @@
 // TODO: implement
+.section .text
+.equ SUSCALL_NUM_RESET_TIMER, 3
+.equ SYSCALL_NUM_SET_TIMER, 4
+hard_clock:
+	**movem.l (レジスタ), -(%SP) /*実行中のタスクのレジスタ退避*/
+	jsr addq /*addqの呼び出し*/
+	jsr sched /*schedの呼びだし*/
+	jsr swtch /*swtchの呼び出し*/
+	**movem.l (%SP)+, (レジスタ)/*レジスタの復帰*/
+	rts
+init_timer:
+	/*タイマのリセットをする*/
+	move.l #SYSCALL_NUM_RESET_TIMER, %D0
+	trap #0
+	/*タイマのセットをする*/
+	move.l #SYSCALL_NUM_SET_TIMER, %D0
+	move.w #10000, %D1 /*1秒に設定*/
+	move.l #hard_clock, %D2 /*hard_clockを呼び出すよう設定*/
+	trap #0
