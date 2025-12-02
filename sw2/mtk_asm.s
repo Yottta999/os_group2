06fb8b3aec79f4cc328556645f671ff8dd52a2e8
k-fog <59814110+k-fog@users.noreply.github.com>
2025-12-02 12:46:54 +0900

    wip

--- sw2/mtk_asm.s
+++ sw2/mtk_asm.s
@@ -1,1 +1,13 @@
-* TODO: implement
+.text
+
+.extern task_tab
+.extern curr_task
+
+.even
+.global first_task
+first_task:
+    * 1. TCB 先頭番地の計算：curr_task の TCB のアドレスを見つける
+    * 2. USP，SSP の値の回復
+    * 3. 残りの全レジスタの回復
+    * 4. ユーザタスクの起動
+    rte
